632200645ab429cc94ca636c6a9fdbebe47969fc
guilhermealvesc <guilherme.092011@hotmail.com>
2024-05-20 21:52:50 -0300

    desafio 1 completo (inicio mnist)

--- 3-desafio/iris.py
+++ 3-desafio/iris.py
@@ -10,14 +10,15 @@ iris['data'] = iris['data'][indices]
 iris['target'] = iris['target'][indices]
 
 iris['target'] = (iris['target'] == 0).astype(int)
-iris
 
 split_size = 0.8
 split_index = int(np.floor(len(iris['data']) * split_size))
 train = {}
 train['data'] = iris['data'][:split_index]
 train['target'] = iris['target'][:split_index]
+train['target'] = train['target'].reshape((train['target'].shape[0], 1))
 
 test = {}
 test['data'] = iris['data'][split_index:]
 test['target'] = iris['target'][split_index:]
+test['target'] = test['target'].reshape((test['target'].shape[0], 1))
